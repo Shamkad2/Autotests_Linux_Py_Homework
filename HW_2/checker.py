dc947cd19dc29dba0360c37c7f308b7997a65761
ShamKad2 <shamilkadyrov@yandex.ru>
2023-10-21 19:13:19 +0300

    add files

---
+++ HW_2/checker.py
@@ -0,0 +1,23 @@
+import subprocess
+from typing import re
+
+
+def check_output(command, text, mode='default'):
+    try:
+        output = subprocess.check_output(command, shell=True).decode()
+        output_words = re.findall(r'\w+', output.lower())
+        if mode == 'default':
+            if text in output:
+                return True
+            else:
+                return False
+        elif mode == 'word':
+            text_words = text.split()
+            for word in text_words:
+                word = word.lower()
+                if word not in output_words:
+                    return False
+            return True
+    except subprocess.CalledProcessError:
+        return False
+
